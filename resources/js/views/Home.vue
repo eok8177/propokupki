ca675dba769c05919d5196ed1b26610e3913031c
Evgen <eok8177@gmail.com>
2019-05-28 23:31:02 +0300

    home page

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -10,46 +10,17 @@
       <div class="container">
         <div class="wrap">
 
-          <div class="shop">
-            <div class="image">
-              <img src="images/shop-1.jpg" alt="">
-            </div>
-            <p><span class="count">218</span> Магазинов</p>
-            <p><span class="count">147</span> Акций</p>
-            <p class="discount">Скидки до <span class="number">-50%</span></p>
+          <div class="shop" v-for="shop in shops">
+            <a href="#">
+              <div class="image">
+                <img :src="shop.image" :alt="shop.title">
+              </div>
+              <p><span class="count">{{shop.shops}}</span> Магазинов</p>
+              <p><span class="count">{{shop.actions}}</span> Акций</p>
+              <p class="discount">Скидки до <span class="number">{{shop.discount}}</span></p>
+            </a>
           </div>
-          <div class="shop">
-            <div class="image">
-              <img src="images/shop-1.jpg" alt="">
-            </div>
-            <p><span class="count">218</span> Магазинов</p>
-            <p><span class="count">147</span> Акций</p>
-            <p class="discount">Скидки до <span class="number">-50%</span></p>
-          </div>
-          <div class="shop">
-            <div class="image">
-              <img src="images/shop-1.jpg" alt="">
-            </div>
-            <p><span class="count">218</span> Магазинов</p>
-            <p><span class="count">147</span> Акций</p>
-            <p class="discount">Скидки до <span class="number">-50%</span></p>
-          </div>
-          <div class="shop">
-            <div class="image">
-              <img src="images/shop-1.jpg" alt="">
-            </div>
-            <p><span class="count">218</span> Магазинов</p>
-            <p><span class="count">147</span> Акций</p>
-            <p class="discount">Скидки до <span class="number">-50%</span></p>
-          </div>
-          <div class="shop">
-            <div class="image">
-              <img src="images/shop-1.jpg" alt="">
-            </div>
-            <p><span class="count">218</span> Магазинов</p>
-            <p><span class="count">147</span> Акций</p>
-            <p class="discount">Скидки до <span class="number">-50%</span></p>
-          </div>
+
           <div class="shop">
             <div class="ico ico-bag"></div>
             <p class="title">Более 230</p>
@@ -57,7 +28,6 @@
             <hr>
             <button class="btn btn-red">Все магазины</button>
           </div>
-
 
         </div>
       </div>
@@ -67,11 +37,43 @@
       <div class="container">
         <h2 class="block-title">Лучшие акции Киева</h2>
         <div class="row">
-          <div class="col-sm-6 col-md-3">
+          <div class="col-sm-6 col-md-3" v-for="action in actions">
             <div class="item">
-              
+              <div class="shop">
+                <div class="image">
+                  <img :src="action.shop.image" :alt="action.title">
+                </div>
+                <div class="right">
+                  <div class="discount">Скидка <span>{{action.shop.discount}}</span></div>
+                  <div class="dates">{{action.shop.discount}}</div>
+                </div>
+              </div>
+              <div class="product">
+                <hr>
+                <div class="image">
+                  <img :src="action.image" :alt="action.title">
+                </div>
+                <span class="title">{{action.title}}</span>
+                <span class="desc">{{action.desc}}</span>
+                <span class="tara">{{action.tara}}</span>
+                <hr>
+                <div class="prices">
+                  <span class="new">{{action.price}}</span>
+                  <span class="old">{{action.oldprice}}</span>
+                </div>
+                <div v-if="action.count > 1" class="sticker">
+                  Осталось {{action.count}} дней
+                </div>
+                <div v-if="action.count == 1" class="sticker last">
+                  Последний день
+                </div>
+              </div>
             </div>
           </div>
+        </div>
+
+        <div class="pagination-row">
+          <button class="btn btn-red">Все акции</button>
         </div>
       </div>
     </div>
@@ -80,10 +82,34 @@
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
   name: 'Home',
-  components: {
-  }
+  data() {
+    return {
+        shops: [],
+        actions: [],
+    }
+  },
+  created: function() {
+    axios.get('/api/shops')
+      .then(
+        (response) => {
+          this.shops = response.data;
+        }
+      )
+      .catch(
+        (error) => console.log(error)
+      );
+    axios.get('/api/actions')
+      .then(
+        (response) => {
+          this.actions = response.data;
+        }
+      )
+      .catch(
+        (error) => console.log(error)
+      );
+  },
 }
 </script>
