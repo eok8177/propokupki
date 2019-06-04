571f0f714fc2dd5b3e587c68efa5ce37640ab6f3
Evgen <eok8177@gmail.com>
2019-06-04 23:11:00 +0300

    cutups: actionEdit (use VueJS)

--- resources/views/cutup/admin/actionadd.blade.php
+++ resources/views/cutup/admin/actionadd.blade.php
@@ -7,7 +7,7 @@
 </div>
 <hr>
 
-<div class="actions">
+<div class="actions" id="actionsForm">
 <form action="">
   <div class="container">
 
@@ -58,16 +58,16 @@
   <div class="container">
     <div class="products" id="products">
 
-      @for ($i = 0; $i <= 3; $i++)
-      <div class="item row">
-        <span class="gray-title">Товар {{$i}}</span><button class="btn-delete"></button>
+      <div class="item row" v-for="product in products">
+        <span class="gray-title">Товар @{{product.id}}</span>
+        <button class="btn-delete" v-on:click.stop.prevent="onDeleteProduct(product.id)"></button>
         <div class="col-md-4 col-xl-6">
           <div class="form-group">
             <label>Фото</label>
             <p>Загрузите фото товара</p>
             <div class="files-input">
-              <img src=""/>
-              <input type="file" class="form-control input-img" multiple="">
+              <img :src="product.image"/>
+              <input type="file" class="form-control input-img">
               <span class="btn-red">Добавить фото</span>
               <span class="text">Или перетащите его сюда</span>
             </div>
@@ -77,14 +77,14 @@
           <div class="form-group col-xl-8 pl-0">
             <label>Название</label>
             <p>Укажите название товара</p>
-            <input type="text" class="form-control" placeholder="Название">
+            <input type="text" class="form-control" placeholder="Название" v-model="product.title">
           </div>
 
           <div class="form-group col-xl-8 pl-0 units-select">
             <label>Количество и вес</label>
             <p>Укажите количество или вес продукта (шт, л, кг)</p>
             <div class="fields">
-              <input type="text" class="form-control" placeholder="Количество или вес">
+              <input type="text" class="form-control" placeholder="Количество или вес" v-model="product.count">
               <div class="select">
                 <select class="custom-select" name="" id="">
                   <option selected="selected">шт</option>
@@ -100,14 +100,13 @@
             <label>Цена</label>
             <p>Укажите старую цену и размер скидки, новая цена посчитается автоматически</p>
             <div class="fields">
-              <input type="text" class="form-control price" placeholder="Старая цена грн">
-              <input type="text" class="form-control discount" placeholder="Размер скидки %">
-              <span class="new-price">Новая цена: <span class="result"></span></span>
+              <input type="text" class="form-control price" placeholder="Старая цена грн" v-model="product.price">
+              <input type="text" class="form-control discount" placeholder="Размер скидки %" v-model="product.discount">
+              <span class="new-price">Новая цена: <span class="result">@{{(product.price - product.price * product.discount / 100).toFixed(2)}}</span></span>
             </div>
           </div>
         </div>
       </div>
-      @endfor
 
     </div>
   </div>
@@ -121,7 +120,7 @@
       <p>Загрузите фото товара</p>
       <div class="files-input">
         <img src=""/>
-        <input type="file" class="form-control input-img" multiple="">
+        <input type="file" id="file" ref="file" class="form-control input-img" v-on:change="handleFileUpload()">
         <span class="btn-red">Добавить фото</span>
         <span class="text">Или перетащите его сюда</span>
       </div>
@@ -130,14 +129,14 @@
     <div class="form-group col-md-4 pl-0">
       <label>Название</label>
       <p>Укажите название товара</p>
-      <input type="text" class="form-control" placeholder="Название">
+      <input type="text" class="form-control" placeholder="Название" v-model="title">
     </div>
 
     <div class="form-group col-md-4 pl-0 units-select">
       <label>Количество и вес</label>
       <p>Укажите количество или вес продукта (шт, л, кг)</p>
       <div class="fields">
-        <input type="text" class="form-control" placeholder="Количество или вес">
+        <input type="text" class="form-control" placeholder="Количество или вес" v-model="count">
         <div class="select">
           <select class="custom-select" name="" id="">
             <option selected="selected">шт</option>
@@ -153,19 +152,23 @@
       <label>Цена</label>
       <p>Укажите старую цену и размер скидки, новая цена посчитается автоматически</p>
       <div class="fields">
-        <input type="text" class="form-control price" placeholder="Старая цена грн">
-        <input type="text" class="form-control discount" placeholder="Размер скидки %">
+        <input type="text" class="form-control price" placeholder="Старая цена грн" v-model="price">
+        <input type="text" class="form-control discount" placeholder="Размер скидки %" v-model="discount">
         <span class="new-price">Новая цена: <span class="result"></span></span>
       </div>
     </div>
 
   </div>
 
+  <div class="container">
+    <button class="btn btn-red" v-on:click.stop.prevent="onAddProduct">Добавить товар</button>
+  </div>
+
 {{-- Кнопки сохранения --}}
   <div class="container">
     <div class="form-group">
       <div class="submit-btns">
-        <button class="btn btn-red" type="submit">Добавить акцию</button>
+        <button class="btn btn-red" type="submit">Обновить акцию</button>
         <button class="btn btn-white">Отмена</button>
       </div>
     </div>
@@ -185,6 +188,10 @@
 @push('scripts')
 <script src="{{ asset('vendor/datepicker/js/bootstrap-datepicker.min.js') }}"></script>
 <script src="{{ asset('vendor/datepicker/locales/bootstrap-datepicker.ru.min.js') }}"></script>
+<!-- VueJS -->
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
 <script>
   $(function() {
     $('.date input').datepicker({
@@ -203,5 +210,72 @@
     });
 
   });
+/* Vue from here )) */
+  var app = new Vue({
+    el: '#actionsForm',
+    data() {
+        return {
+          products: [],
+          actionID: 1, //set from laravel
+          img: '',
+          title: '',
+          count: '',
+          price: '',
+          discount: ''
+        }
+    },
+    created: function() {
+      axios.get('/api/admin-actions/'+this.actionID)
+        .then(
+          (response) => {
+            this.products = response.data;
+          }
+        )
+        .catch(
+          (error) => console.log(error)
+        );
+    },
+    methods: {
+      handleFileUpload(){
+        this.img = this.$refs.file.files[0];
+      },
+      onAddProduct(event) {
+        const formData = new FormData();
+        formData.append('image', this.img);
+        formData.append('title', this.title);
+        formData.append('count', this.count);
+        formData.append('price', this.price);
+        formData.append('discount', this.discount);
+        const config = {
+          headers: {
+            'content-type': 'multipart/form-data'
+          }
+        };
+        axios.post('/api/admin-actions/'+this.actionID, formData, config)
+          .then(
+            (response) => {
+              this.products = response.data;
+            }
+          )
+          .catch(
+            (error) => console.log(error)
+          );
+      },
+      onDeleteProduct(product) {
+        if(confirm("Do you really want to delete?")){
+          axios.delete('/api/admin-actions/'+this.actionID+'/'+product)
+            .then(
+              (response) => {
+                this.products = response.data;
+              }
+            )
+            .catch(
+              (error) => console.log(error)
+            );
+          }
+      },
+
+    }
+  })
 </script>
 @endpush
