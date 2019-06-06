275fd08a036d339582bd7a48917dd04d562cead4
Evgen <eok8177@gmail.com>
2019-06-06 18:33:16 +0300

    admin css fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,19 @@
 const mix = require('laravel-mix');
 
+mix.webpackConfig({ devtool: "inline-source-map" });
+
 mix
   // admin
   //.js('resources/js/admin.js', 'public/js/admin.js')
-  // .sass('resources/sass/admin.scss', 'public/css/admin.css')
+  .sass('resources/sass/admin.scss', 'public/css/admin.css')
 
   // .styles('public/css/style.css', 'public/css/style.min.css')
 
   // front
   .js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css/app.css')
-;
+
+  .sourceMaps();
 
 mix.options({
   processCssUrls: false
