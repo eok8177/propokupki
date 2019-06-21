f9e7625db76b32327718c0a1c85569e83c63a7ab
Evgen <eok8177@gmail.com>
2019-06-21 23:07:44 +0300

    add Pagination

--- resources/js/views/Actions.vue
+++ resources/js/views/Actions.vue
@@ -7,7 +7,7 @@
 
     <filter-top></filter-top>
 
-    <products :products="actions"></products>
+    <products :products="actions" :pages="pages" :count="count"></products>
 
   </div>
 </template>
@@ -26,6 +26,8 @@ export default {
     return {
       filter: '',
       actions: [],
+      pages: 10, //count pages in paginate
+      count: 12, //count items on page
     }
   },
   methods: {
@@ -42,6 +44,9 @@ export default {
         .catch(
           (error) => console.log(error)
         );
+    },
+    paginate: function(e) {
+      console.log(e);
     }
   },
   created: function() {
