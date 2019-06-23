3226219338763f9e162778d0961165a1768d3c1c
Evgen <eok8177@gmail.com>
2019-06-23 22:57:03 +0300

    Cabinet; About; Contact

---
+++ resources/js/views/About.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="product-page">
+
+    <div class="container page-title">
+      <h1 class="title">О нас</h1>
+    </div>
+
+    <div class="text-page">
+      <div class="container">
+        <h2>О проекте</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut nesciunt illo praesentium, ab! Doloremque eius quae accusantium, quod quo cupiditate sunt, sit in magnam animi quidem facere nesciunt saepe ex.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut nesciunt illo praesentium, ab! Doloremque eius quae accusantium, quod quo cupiditate sunt, sit in magnam animi quidem facere nesciunt saepe ex.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut nesciunt illo praesentium, ab! Doloremque eius quae accusantium, quod quo cupiditate sunt, sit in magnam animi quidem facere nesciunt saepe ex.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut nesciunt illo praesentium, ab! Doloremque eius quae accusantium, quod quo cupiditate sunt, sit in magnam animi quidem facere nesciunt saepe ex.</p>
+      </div>
+    </div>
+
+
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'About',
+  components: {
+  },
+  data() {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
