8f1d6c88670a9deb4451a86daaf78859a5816d5b
Evgen <eok8177@gmail.com>
2019-08-21 15:21:48 +0300

    fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,11 @@
 const mix = require('laravel-mix');
 
-mix.webpackConfig({ devtool: "inline-source-map" });
+// mix.webpackConfig({ devtool: "inline-source-map" });
 
 mix
   // admin
   //.js('resources/js/admin.js', 'public/js/admin.js')
-  .sass('resources/sass/admin.scss', 'public/css/admin.css')
+  // .sass('resources/sass/admin.scss', 'public/css/admin.css')
 
   // .styles('public/css/style.css', 'public/css/style.min.css')
 
@@ -13,7 +13,7 @@ mix
   .js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css/app.css')
 
-  .sourceMaps();
+  // .sourceMaps();
 
 mix.options({
   processCssUrls: false
